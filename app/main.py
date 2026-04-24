b6a7eb50cd1975e47e911b5edad2d96801df7546
Robert Germain <142847006+Robertgermain@users.noreply.github.com>
2026-04-24 17:26:19 -0400

    Added request and response models with validation for the tickets APIs

--- app/main.py
+++ app/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+# Empty list to store tickets. (This will be converted to a database later)
 tickets = []
+
+
+# Base model for creating a new ticket from user input
+class TicketCreate(BaseModel):
+    title: str
+
+
+# Base model for returning the ticket data once created
+class Ticket(BaseModel):
+    id: int
+    title: str
+    status: str
 
 
 @app.get("/")
@@ -10,13 +24,14 @@ async def test():
     return {"message": "AI Ticket Processing System is running"}
 
 
-@app.get("/tickets")
+@app.get("/tickets", response_model=list[Ticket])
 async def get_tickets():
     return tickets
 
 
 @app.post("/tickets")
-async def create_ticket():
-    ticket = {"id": len(tickets) + 1, "title": "Sample Ticket", "status": "open"}
-    tickets.append(ticket)
-    return ticket
+async def create_ticket(ticket: TicketCreate, response_model=Ticket):
+    # Auto-generates ID and sets default ticket status
+    new_ticket = {"id": len(tickets) + 1, "title": ticket.title, "status": "open"}
+    tickets.append(new_ticket)
+    return new_ticket
